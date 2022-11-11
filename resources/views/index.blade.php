6b088e2e39e102fcbc11406e4b4ab4320eaad02c
Ma. Leonora Mendenilla <maleonora.mendenilla@gmail.com>
2022-11-11 09:23:18 +0800

    update 202221111 About UI Implementation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -65,20 +65,21 @@
 
     {{--content --}}
 
-    <div class="row">
+    <div class="row w-75 mt-1" >
         <div class=" col-12 col-lg-6 mt-5 text-center ">
             <div class="card w-50 h-75 mx-auto">
                 <div class="w-100 overflow-hidden mx-auto h-75">
                     <img src="{{ asset('uploads/settings/doc.jpg') }}" class="img img-fluid m-auto " style="object-position: center; transform: translateY(-15%);">
                 </div>
-                <h3 class="card-title mt-2">Silverio E. Nano, D.V.M </h3>
+                <h5 class="card-title mt-2">Silverio E. Nano, D.V.M </h5>
                 <h4 class="card-text">Veterinarian</h4>
                 <p class="card-text font-italic">since 2006</p>
             </div>
         </div>
 
-        <div class=" col-12 col-lg-6 mt-3 d-flex text-center ">
-            <p class="card-text font-italic text-white"></p>
+        <div class=" col-12 col-lg-6 mt-3 d-flex text-center"style="border-left: solid white;">
+            <p class="card-text font-italic text-white mx-auto mt-3"><br><br><br>Dr. Silverio E. Nano specializes in the area of Dog and Cat. He is interested in Clinical/Surgical procedures. The Nano Veterinary Clinic was built last September 2015. Our Veterinary Clinic was located in Glendale Village, Brgy. Dolores, Taytay Rizal. If you have concerns for your pets health you can look for Doc. Nano and his staffs. We would loved to accomodate you and your pets, and answers all your questions and inquiries.<br><br><h7 class="font-weight-bold">Our Patients:</h7><br> <h9><i class="fa-solid fa-dog mr-2"></i>Dog</h9> <br><h9><i class="fa-solid fa-cat mr-2"></i>Cat</h9> </p>
+
         </div>
     </div>
 </div>
