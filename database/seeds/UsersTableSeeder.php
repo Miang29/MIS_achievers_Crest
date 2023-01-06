4edecd1f45b4a9ceadb5c1e266043a8b336a32a0
Virus5600 <satchi5600@gmail.com>
2023-01-06 12:42:38 +0800

    Patch 20230106 - Procfile Patch

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,22 +5,21 @@ use App\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        User::create (
-            [
-                'first_name' => 'Ma.Leonora',
-                'middle_name' => 'Gonzales',
-                'last_name' => 'Mendenilla',
-                'username'=> 'admin',
-                'email' =>'maleonora.mendenilla@gmail.com',
-                'password' => Hash::make('admin123'),
-                'user_type_id' => 1
-            ]); 
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		User::create ([
+			'first_name' => 'Ma.Leonora',
+			'middle_name' => 'Gonzales',
+			'last_name' => 'Mendenilla',
+			'username'=> 'admin',
+			'email' =>'maleonora.mendenilla@gmail.com',
+			'password' => Hash::make('admin123'),
+			'user_type_id' => 1
+		]);
+	}
 }
